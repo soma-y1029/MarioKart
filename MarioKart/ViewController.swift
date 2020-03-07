157abe6f96b76c26a6fd10e0a2fb5ff3171bccb3
soma-y1029 <57809819+soma-y1029@users.noreply.github.com>
2020-03-06 16:21:59 -0800

    Initial Commit

---
+++ MarioKart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarioKart
+//
+//  Created by Soma Yoshida on 3/6/20.
+//  Copyright © 2020 Soma Yoshida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
